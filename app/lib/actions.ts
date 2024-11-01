af6b8a8509e08f38b59b2adf51439a51ed60aaae
EricRCorral <eric-wil@hotmail.com>
2024-11-01 11:05:06 -0300

    Lint errors fixed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,7 +54,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -82,7 +82,7 @@ export async function updateInvoice(id: string, formData: FormData) {
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
       `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Update Invoice.",
     };
@@ -97,7 +97,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
